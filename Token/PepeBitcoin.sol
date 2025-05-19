29a114e8893bb139333de7a615b72210d7de0ac2
Sebastian Salmhofer <sebastian@salmhofer.cc>
2025-05-19 17:28:28 -0300

    Update PepeBitcoin.sol

--- Token/PepeBitcoin.sol
+++ Token/PepeBitcoin.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.27;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+/// @custom:security-contact admin@pepe-bitcoin.com
+contract PepeBitcoin is ERC20, ERC20Permit {
+    constructor(address recipient)
+        ERC20("PepeBitcoin", "PBTC")
+        ERC20Permit("PepeBitcoin")
+    {
+        _mint(recipient, 100000000 * 10 ** decimals());
+    }
+}
 
